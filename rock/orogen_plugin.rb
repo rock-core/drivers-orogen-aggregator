4c2b4cddc89c53ae41930d4e478078e5357027dc
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2011-08-31 11:24:24 +0200

    orogen_plugin: documentation

--- rock/orogen_plugin.rb
+++ rock/orogen_plugin.rb
@@ -1,4 +1,12 @@
+# General underlying support for code generation where data gets pulled from the
+# ports
+#
+# To pull data from ports, one needs to have a single loop where the data get
+# read and then dispatched to all registered listeners. This plugin provides the
+# underlying capability of registering the listeners, and then generate the code
+# to push data to all of them
 module PortListenerPlugin
+    # The extension that gets registered on TaskContext
     class Extension
 	attr_reader :in_loop_code
         attr_reader :port_listeners
@@ -9,6 +17,9 @@ module PortListenerPlugin
 	end
 	
         def register_for_generation(task)
+            # Use a block so that the code generation gets delayed. This makes
+            # sure that all listeners are registered properly before we generate
+            # the code
 	    task.in_base_hook("update") do
 	    code = "
     bool keepGoing = true;
@@ -66,7 +77,10 @@ module PortListenerPlugin
     end
 end
 
+# Support for the usage of the stream aligner in oroGen 
 module StreamAlignerPlugin
+    # Class that takes care of the C++ code generation from a stream aligner
+    # specification
     class Generator
 	def generate_port_listener_code(task, config)
             port_listener_ext = task.extension("port_listener")
@@ -147,6 +161,7 @@ module StreamAlignerPlugin
     #{agg_name}.clear();
     ")
 	end
+	
 	def type_cxxname(stream, task)
 	    port = task.find_input_port(stream.port_name)
 	    if(!port)
@@ -157,19 +172,40 @@ module StreamAlignerPlugin
 	end
     end
 
+    # Definition of a single stream
     class Stream
-	def initialize(name, period)
+        # Adds a new stream. +name+ is the task's port name and +period+ the
+        # default period
+        #
+        # It is strongly advised to keep the period to zero
+	def initialize(name, period = 0)
 	    @port_name = name
 	    @data_period = period
 	end
 
+        # The task's port name
 	attr_reader :port_name
+        # The period of the incoming data. It is strongly advised to keep it to
+        # zero in stream aligner specifications
 	attr_reader :data_period
     end
     
+    # Extension to the task model to represent the stream aligner setup
     class Extension
+        # The stream aligner name. Always "aggregator" for now
 	attr_reader :name
+
+        ##
+        # :method: max_latency
+        # :call-seq:
+        #   max_latency => value
+        #   max_latency new_value => new_value
+        #
+        # Gets or sets the stream aligner's default maximum latency. It can also
+        # be set at runtime using the aggregator_max_latency property
 	dsl_attribute :max_latency	
+
+        # The defined streams, as an array of Stream objects
 	attr_reader :streams
 	
 	def initialize()
@@ -177,10 +213,16 @@ module StreamAlignerPlugin
             @name = "aggregator"
 	end
 	
+        # Declares that the stream aligner should be configured to pull data
+        # from the input port +name+ with a default period of +default_period+
+        #
+        # Periods are highly system-specific. It is very stronly advised to keep
+        # it to the default value of zero.
 	def align_port(name, default_period = 0)
 	    streams << Stream.new(name, default_period)
 	end	
 
+        # Adds to the task the interface objects for the benefit of the stream aligner
         def update_spec(task)
 	    task.project.import_types_from('aggregator')
 
@@ -214,6 +256,20 @@ end
 
 
 class Orocos::Spec::TaskContext
+    # Adds a stream aligner to this task
+    #
+    # See http://rock-robotics.org/documentation/data_processing/index.html for
+    # a general introduction
+    #
+    # The new stream aligner object is an instance of
+    # StreamAlignerPlugin::Extension and can be retrieved with
+    #
+    #   task_model.stream_aligner
+    #
+    # Or, more generically,
+    #
+    #   task_model.extension("stream_aligner")
+    #
     def stream_aligner(&block)	
         PortListenerPlugin.add_to(self)
 
