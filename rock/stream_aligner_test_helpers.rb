5f824075dc51b53dd1fa45028c70778178ed86a1
higor-melo <higor.melo@tidewise.io>
2024-10-20 10:28:13 -0300

    feat: add stream_aligner_test_helpers
    Stream aligner tests need to check if the
    sample values time_arg are equal between the
    written sample and the read sample

---
+++ rock/stream_aligner_test_helpers.rb
@@ -0,0 +1,39 @@
+# frozen-string-literal: true
+
+# no-doc
+module Aggregator
+    # Test helpers for stream aligner
+    module TestHelpers
+        # write `sample` to `port` and waits until status reader `latest_time` equals
+        # sample time
+        def stream_aligner_write(task, port, sample, time_arg = timestamp, &block)
+            reader = stream_aligner_status_reader(task)
+            expect_execution do
+                syskit_write port, sample
+            end.to do
+                have_one_new_sample(reader).matching do |t|
+                    send(t, ".#{time_arg}") == send(sample, ".#{time_arg}")
+                end
+                instance_eval(&block) if block # let the caller add more predicates
+            end
+        end
+
+        def stream_aligner_status_reader(task)
+            @stream_aligner_status_reader[task] ||=
+                syskit_create_reader(task.transformer_stream_aligner_status,
+                                     type: :buffer, size: 20)
+        end
+
+        # setup called in minitest's before block
+        def setup
+            @stream_aligner_status_reader = {}
+            super
+        end
+
+        # teardown called in minitest's after block
+        def teardown
+            @stream_aligner_status_reader.each_value(&:disconnect)
+            super
+        end
+    end
+end
