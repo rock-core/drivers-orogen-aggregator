231adf0680affc4339acdeab85e435c7fa7cc311
higor-melo <higor.melo@tidewise.io>
2024-10-24 14:30:52 -0300

    fix: compare time without max-latency
    This was wrong, the time must be exactly the same.
    Add checks for reader creation.
    Fix installation path.

--- rock/stream_aligner_test_helpers.rb
+++ rock/stream_aligner_test_helpers.rb
@@ -8,41 +8,51 @@ module Syskit
             # write `sample` to `port` and waits until status reader `latest_time` equals
             # sample time
             def stream_aligner_write(
-                task,
-                port,
-                sample,
-                sample_time_field: "timestamp",
-                port_time_field: "timestamp",
-                max_latency: 0.1,
-                &block
+                task, port, sample, sample_time_field: "timestamp", &block
             )
                 reader = stream_aligner_status_reader(task)
                 writer = syskit_create_writer port
-                expect_execution.timeout(15).poll do
-                    writer.write(sample)
-                end.to do
-                    have_one_new_sample(reader).matching do |t|
-                        (t[port_time_field] - sample[sample_time_field])
-                            .abs <= max_latency
+                expect_execution { writer.write(sample) }
+                    .to do
+                        have_one_new_sample(reader).matching do |t|
+                            t.latest_time == sample[sample_time_field]
+                        end
+                        # let the caller add more predicates
+                        instance_eval(&block) if block
                     end
-                    instance_eval(&block) if block # let the caller add more predicates
-                end
             end
 
             def stream_aligner_status_reader(task)
+                property_name, port_name =
+                    if task.properties.include?(:transformer_status_period)
+                        # Transformer task
+                        %I[transformer_status_period
+                           transformer_stream_aligner_status]
+                    else
+                        # Pure stream aligner task
+                        %I[stream_aligner_status_period stream_aligner_status]
+                    end
+
+                if task.property(property_name).read != 0
+                    raise ArgumentError,
+                          "The stream aligner task needs to have a zero " \
+                          "status_period for the test helpers to work"
+                end
+
                 @stream_aligner_status_reader[task] ||=
-                    syskit_create_reader(task.transformer_stream_aligner_status_port,
-                                         type: :buffer, size: 20)
+                    syskit_create_reader(
+                        task.find_port(port_name), type: :buffer, size: 20
+                    )
             end
 
             # setup called in minitest's before block
-            def stream_aligner_reader_setup
+            def setup
                 @stream_aligner_status_reader = {}
                 super
             end
 
             # teardown called in minitest's after block
-            def stream_aligner_reader_teardown
+            def teardown
                 @stream_aligner_status_reader.each_value(&:disconnect)
                 super
             end
